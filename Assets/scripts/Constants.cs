c6a06c6a687292acd11e6dab3b3a4bded3451f6c
Vishal <vishaljaiswal5000@gmail.com>
2023-04-10 13:35:19 +1000

    Added: Level selection Canvas

---
+++ Assets/scripts/Constants.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants
+{
+
+   public const string TAG_TUTORIAL = "Level_Tutorial";
+   public const string TAG_LEVEL1 = "Level_1";
+   public const string TAG_LEVEL2 = "Level_2";
+   public const string TAG_LEVEL3 = "Level_3";
+   public const string TAG_LEVEL4 = "Level_4";
+
+
+    public const string SCENE_TUTORIAL = "Demo";
+    public const string SCENE_LEVEL1 = "Level1";
+    public const string SCENE_LEVEL2 = "Level2";
+    public const string SCENE_LEVEL3 = "Level3";
+    public const string SCENE_LEVEL4 = "Level4";
+
+
+}
--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
@@ -8,33 +9,37 @@ public class MainMenu : MonoBehaviour
     private GameObject mainMenu;
     private GameObject loading;
     private GameObject tutorial;
-    
+    private GameObject level;
+
 
     void Start()
     {
         mainMenu = GameObject.Find("MainMenuCanvas");
         loading = GameObject.Find("LoadingCanvas");
         tutorial = GameObject.Find("TutorialCanvas");
+        level = GameObject.Find("LevelSelectionCanvas");
 
         mainMenu.GetComponent<Canvas>().enabled = true;
         loading.GetComponent<Canvas>().enabled = false;
         tutorial.GetComponent<Canvas>().enabled = false;
+        level.GetComponent<Canvas>().enabled = false;
+
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
 
     public void StartButton()
     {
-        loading.GetComponent<Canvas>().enabled = true;
+        loading.GetComponent<Canvas>().enabled = false;
+        level.GetComponent<Canvas>().enabled = true;
         mainMenu.GetComponent<Canvas>().enabled = false;
         tutorial.GetComponent<Canvas>().enabled = false;
-        SceneManager.LoadScene("Demo");
     }
 
     public void TutorialButton()
@@ -56,4 +61,34 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void LevelSelection(Button button)
+    {
+        loading.GetComponent<Canvas>().enabled = true;
+        level.GetComponent<Canvas>().enabled = false;
+        mainMenu.GetComponent<Canvas>().enabled = false;
+        tutorial.GetComponent<Canvas>().enabled = false;
+
+        if (button.CompareTag(Constants.TAG_TUTORIAL))
+        {
+            SceneManager.LoadScene(Constants.SCENE_TUTORIAL);
+        }
+        else if (button.CompareTag(Constants.TAG_LEVEL1))
+        {
+            SceneManager.LoadScene(Constants.SCENE_LEVEL1);
+        }
+        else if (button.CompareTag(Constants.TAG_LEVEL2))
+        {
+            SceneManager.LoadScene(Constants.SCENE_LEVEL2);
+        }
+        else if (button.CompareTag(Constants.TAG_LEVEL3))
+        {
+            SceneManager.LoadScene(Constants.SCENE_LEVEL3);
+        }
+        else if (button.CompareTag(Constants.TAG_LEVEL4))
+        {
+            SceneManager.LoadScene(Constants.SCENE_LEVEL4);
+        }
+    }
+
 }
