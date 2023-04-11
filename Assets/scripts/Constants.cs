4f9bf77bf7c628ee7822a0e6995daa9a3671d6d9
Vishal <vishaljaiswal5000@gmail.com>
2023-04-11 13:44:39 +1000

    Added: Level 4 world Fixes: raycast and door motion technique

--- Assets/scripts/Constants.cs
+++ Assets/scripts/Constants.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Constants
 {
+    // Layers
+    public const string LAYER_INTERACTABLE = "Interactable";
+    
 
-   public const string TAG_TUTORIAL = "Level_Tutorial";
-   public const string TAG_LEVEL1 = "Level_1";
-   public const string TAG_LEVEL2 = "Level_2";
-   public const string TAG_LEVEL3 = "Level_3";
-   public const string TAG_LEVEL4 = "Level_4";
+    // Tags
+    public const string TAG_TUTORIAL = "Level_Tutorial";
+    public const string TAG_LEVEL1 = "Level_1";
+    public const string TAG_LEVEL2 = "Level_2";
+    public const string TAG_LEVEL3 = "Level_3";
+    public const string TAG_LEVEL4 = "Level_4";
+    public const string TAG_DOOR = "Door";
 
-
+    // Scenes
     public const string SCENE_TUTORIAL = "Demo";
     public const string SCENE_LEVEL1 = "Level_1";
     public const string SCENE_LEVEL2 = "Level_2";
